932376e9f33d955d311254a32983a5704ec63c27
Oleg Borschev <bosoleg89@gmail.com>
2019-04-02 22:37:17 +0300

    Fixed condition in task-2 hw2

---
+++ hw2/task-2/app.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const passwords = ["qwerty", "111qwe", "123123", "r4nd0mp4zzw0rd"];
+let attemptsLeft = 3;
+let input;
+
+do {
+  input = prompt("Enter password");
+  if (!passwords.includes(input)) {
+    attemptsLeft -= 1;
+    if (attemptsLeft == 0) {
+      alert("Your attempts have ended, your account has been blocked!");
+      break;
+    }
+    alert(`Incorrect password! You have ${attemptsLeft} attemps`);
+    continue;
+  } else {
+    alert("Welcome");
+    break;
+  }
+} while (input);
+
+// do {
+//   input = prompt("Enter password");
+//   if (passwords.includes(input)) {
+//     alert("Welcome");
+//     break;
+//   } else if (!passwords.includes(input)) {
+//     attemptsLeft -= 1;
+//     if (attemptsLeft == 0) {
+//       alert("Your attempts have ended, your account has been blocked!");
+//       break;
+//     }
+//     alert(`Incorrect password! You have ${attemptsLeft} attemps`);
+//     continue;
+//   }
+// } while (input);
