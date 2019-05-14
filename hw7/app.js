65a3df02cda9a96fff56c167906808b64fc705ba
Oleg Borschev <bosoleg89@gmail.com>
2019-05-15 00:38:25 +0300

    Added home work 7

---
+++ hw7/app.js
@@ -0,0 +1,192 @@
+"use script";
+
+const users = [
+  {
+    id: "701b29c3-b35d-4cf1-a5f6-8b12b29a5081",
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    skills: ["ipsum", "lorem"],
+    gender: "male",
+    age: 37
+  },
+  {
+    id: "7a3cbd18-57a1-4534-8e12-1caad921bda1",
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
+    gender: "female",
+    age: 34
+  },
+  {
+    id: "88beb2f3-e4c2-49f3-a0a0-ecf957a95af3",
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    skills: ["nulla", "anim", "proident", "ipsum", "elit"],
+    gender: "male",
+    age: 24
+  },
+  {
+    id: "249b6175-5c30-44c6-b154-f120923736f5",
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    skills: ["adipisicing", "irure", "velit"],
+    gender: "female",
+    age: 21
+  },
+  {
+    id: "334f8cb3-eb04-45e6-abf4-4935dd439b70",
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong"],
+    isActive: true,
+    balance: 3951,
+    skills: ["ex", "culpa", "nostrud"],
+    gender: "male",
+    age: 27
+  },
+  {
+    guid: "150b00fb-dd82-427d-9faf-2879ea87c695",
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman"],
+    isActive: false,
+    balance: 1498,
+    skills: ["non", "amet", "ipsum"],
+    gender: "male",
+    age: 38
+  },
+  {
+    id: "e1bf46ab-7168-491e-925e-f01e21394812",
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    skills: ["lorem", "veniam", "culpa"],
+    gender: "female",
+    age: 39
+  }
+];
+
+// Task 1
+const getAllNames = users => {
+  return users.map(value => value.name);
+};
+
+console.table(getAllNames(users));
+
+// Task 2
+const getUsersByEyeColor = (users, color) => {
+  return users.filter(value => value.eyeColor === color);
+};
+
+console.table(getUsersByEyeColor(users, "blue"));
+
+// Task 3
+const getUsersByGender = (users, gender) => {
+  return users.filter(value => value.gender === gender);
+};
+
+console.table(getUsersByGender(users, "male"));
+
+// Task 4
+const getInactiveUsers = users => {
+  return users.filter(value => value.isActive);
+};
+
+console.table(getInactiveUsers(users));
+
+// Task 5
+const getUserByEmail = (users, email) => {
+  return users.filter(value => value.email === email);
+};
+
+console.table(getUserByEmail(users, "shereeanthony@kog.com")); // {объект пользователя Sheree Anthony}
+console.table(getUserByEmail(users, "elmahead@omatom.com")); // {объект пользователя Elma Head}
+
+// Task 6
+const getUsersWithAge = (users, min, max) => {
+  return users.filter(value => value.age > min && value.age < max);
+};
+
+console.table(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
+
+console.table(getUsersWithAge(users, 30, 40));
+// [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
+
+// Task 7
+const getTotalBalance = users => {
+  // твой код
+  return users.reduce((acc, value) => acc + value.balance, 0);
+};
+
+console.log(getTotalBalance(users)); // 20916
+
+// Task 8
+const getUsersByFriend = (users, name) => {
+  // твой код
+  return users.filter(value => value.friends.includes(name));
+};
+
+console.table(getUsersByFriend(users, "Briana Decker")); // [ 'Sharlene Bush', 'Sheree Anthony' ]
+console.table(getUsersByFriend(users, "Goldie Gentry")); // [ 'Elma Head', 'Sheree Anthony' ]
+
+// Additional tasks
+
+// const getUniqueSkills = users => {
+//   // твой код
+//   const allSkills = users.map(
+//     value =>
+//       value.skills !==
+//       users.reduce((acc, value) => {
+//         acc.push(...value.skills);
+//         return acc;
+//       }, [])
+//   );
+//   console.log(allSkills);
+//   allSkills.map(value => value !== )
+
+// allSkills.forEach(value => {
+//     value.includes(allSkills);
+//     return
+// })
+// function getUniq(acc, tag) {
+//     const result = [];
+//   if (!acc.hasOwnProperty(tag)) {
+//       result.push()
+//   }
+// }
+// return allSkills.filter(value => !value.includes(allSkills)).sort();
+// };
+
+// console.log(getUniqueSkills(users));
+// [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
+
+// const getNamesSortedByFriendsCount = users => {
+//   // твой код
+//   return users
+//     .map(value => value.name)
+//     .sort((a, b) => b.friends - a.friends);
+// };
+// console.table(users.map(value => value));
+// console.log(getNamesSortedByFriendsCount(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
